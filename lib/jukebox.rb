bbb864967427907ce38676c8666e58b237d8b7a5
Kyle Tulod <tulod.ky13@gmail.com>
2016-11-29 07:12:53 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,44 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+def help
+puts "  I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+end
 
+def play(songs_array)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if (1..9).to_a.include?(input.to_i)
+    puts "Playing #{songs_array[input.to_i - 1]}"
+  elsif songs_array.include?(input.to_s)
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def list (songs_array)
+  songs_array.each_with_index do |title, number|
+    puts "#{number + 1}. #{title}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run (songs)
+  puts "Please enter a command:"
+  input = gets.chomp
+  case input
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit
+    end
+end
